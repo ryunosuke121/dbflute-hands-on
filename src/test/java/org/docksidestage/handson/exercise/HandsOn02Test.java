b5056fc1adc6ebd33d36aff3cafb396a3077f5fa
ryunosuke.ito <ryunosuke.ito@bizreach.co.jp>
2026-05-12 18:18:50 +0900

    todo消化

--- src/test/java/org/docksidestage/handson/exercise/HandsOn02Test.java
+++ src/test/java/org/docksidestage/handson/exercise/HandsOn02Test.java
@@ -8,7 +8,10 @@ import org.docksidestage.handson.dbflute.exbhv.MemberBhv;
 import org.docksidestage.handson.dbflute.exentity.Member;
 import org.docksidestage.handson.unit.UnitContainerTestCase;
 
-// TODO itoryu 一応、ここでもJavaDocお願いします (authorだけでもいいので) by jflute (2026/04/26)
+// TODO done itoryu 一応、ここでもJavaDocお願いします (authorだけでもいいので) by jflute (2026/04/26)
+/**
+ * @author ryunosuke.ito
+ */
 public class HandsOn02Test extends UnitContainerTestCase {
 
     @Resource
@@ -28,8 +31,8 @@ public class HandsOn02Test extends UnitContainerTestCase {
     public void test_会員名称がSで始まる会員を取得() throws Exception {
         // ## Act ##
         ListResultBean<Member> memberList = memberBhv.selectList(cb -> {
-        	// TODO itoryu わりと定型なので、Lambda引数名は op -> op.likePrefix() みたいでOK by jflute (2026/04/26)
-            cb.query().setMemberName_LikeSearch("S", likeSearchOption -> likeSearchOption.likePrefix());
+        	// TODO done itoryu わりと定型なので、Lambda引数名は op -> op.likePrefix() みたいでOK by jflute (2026/04/26)
+            cb.query().setMemberName_LikeSearch("S", op -> op.likePrefix());
             cb.query().addOrderBy_MemberName_Asc();
         });
 
@@ -42,8 +45,8 @@ public class HandsOn02Test extends UnitContainerTestCase {
         for (Member member : memberList) {
             String memberName = member.getMemberName();
 
-            // TODO itoryu 些細なとこだけど、memberName.startsWith()というメソッドもある by jflute (2026/04/26)
-            assertTrue(memberName.charAt(0) == 'S');
+            // TODO done itoryu 些細なとこだけど、memberName.startsWith()というメソッドもある by jflute (2026/04/26)
+            assertTrue(memberName.startsWith("S"));
             if (prevMemberName != null) {
                 assertTrue(memberName.compareTo(prevMemberName) >= 0);
             }
@@ -95,7 +98,8 @@ public class HandsOn02Test extends UnitContainerTestCase {
         });
 
         // ## Assert ##
-        // TODO itoryu 0のとき、greenになってしまう by jflute (2026/04/28)
+        // TODO done itoryu 0のとき、greenになってしまう by jflute (2026/04/28)
+        assertTrue(!memberList.isEmpty());
 
         // allMatch()のJavaDocを見ると...
         // if the stream is empty then true is returned and the predicate is not evaluated.
